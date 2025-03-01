560ee89d6696fc8b903e6c2afa9668021125b1c6
valclassdevelop <developervalclass.com>
2025-03-01 16:15:27 +0700

    fix: base url

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import store from "../redux/store";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3600',
-  // baseURL: 'https://be-unipay.vercel.app',
+  // baseURL: 'http://localhost:3600',
+  baseURL: 'https://api-unipay.vercel.app',
   headers: {
     "Content-Type": "application/json",
   }
